ec17ac6894051def39c5ed17435a42a31eee9a3a
Yuting Xie <yuting.xie@aalto.fi>
2022-11-14 20:53:24 +0200

    ray, hitable, tracey update

--- include/tracey/ray/ray.hpp
+++ include/tracey/ray/ray.hpp
@@ -17,9 +17,24 @@ class Ray {
   Vector3f get_direction() const { return direction_; }
   Vector3f at(float f) const { return origin_ + direction_ * f; }
 
+  friend inline Vector3f RandomInUnitSphere();
+  friend inline Vector3f Reflect(const Vector3f& v, const Vector3f& n);
+
  private:
   Vector3f origin_;
   Vector3f direction_;
 };
+
+Vector3f RandomInUnitSphere(){
+    Vector3f point;
+    do{
+        point=Vector3f::Random(-1,1);
+    }while (point.norm() >= 1);
+	return point;
+}
+
+inline Vector3f Reflect(const Vector3f& v, const Vector3f& n){
+	return v - 2 * v.dot(n) * n;
+}
 
 }
