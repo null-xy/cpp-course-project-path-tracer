b21799206d78f65cc9679a1a22eabaf80d6639ff
Kääriäinen Aleksi <aleksi.kaariainen@aalto.fi>
2022-11-14 15:00:25 +0200

    restructure under /tracey

--- include/tracey/utils/FileWriterTest.cpp
+++ include/tracey/utils/FileWriterTest.cpp
@@ -6,6 +6,7 @@
 #include "../geometry/Sphere.hpp"
 #include "../ray/Ray.hpp"
 #include "FileWriter.hpp"
+#include "../camera.hpp"
 
 using Eigen::Vector3f;
 
@@ -22,7 +23,7 @@ Vector3f RayColor(Ray& r, const Geometry& scene) {
 }
 
 // compile instructions:
-// FileWriterTest.cpp FileWriter.cpp ../ray/Ray.cpp ../geometry/Geometry.hpp ../geometry/Sphere.cpp ../geometry/GeometryList.cpp -o FileWriterTest.o to
+// g++ FileWriterTest.cpp FileWriter.cpp ../ray/Ray.cpp ../geometry/Geometry.hpp ../geometry/Sphere.cpp ../geometry/GeometryList.cpp ../tracey/camera.hpp -o FileWriterTest.o
 // save as file, run the compiled file with '> o.ppm'
 int main() {
   std::vector<std::vector<Vector3f>> image;
@@ -34,11 +35,11 @@ int main() {
 
   // scene
   GeometryList scene;
-  scene.Add(std::make_shared<Sphere>(Vector3f(0.0, -100.5, -1.0), 100));
-  scene.Add(std::make_shared<Sphere>(Vector3f(0.0, 0.0, -1.0), 0.5));
+  //scene.Add(std::make_shared<Sphere>(Vector3f(0.0, -100.5, -1.0), 100));
+  scene.Add(std::make_shared<Sphere>(Vector3f(0.0, 0.0, -1), 0.5));
 
   // camera
-  auto viewPortHeight = 2.0;
+  /*auto viewPortHeight = 2.0;
   auto viewportWidth = aspectRatio * viewPortHeight;
   auto focalLength = 1.0;
 
@@ -47,13 +48,14 @@ int main() {
   Vector3f vertical(0.0, viewPortHeight, 0.0);
   Vector3f bottomLeftCorner =
       origin - horizontal / 2 - vertical / 2 - Vector3f(0.0, 0.0, focalLength);
+  */
+
+  Tracey::Camera cam(width, aspectRatio * width, 60.0);
 
   for (int j = height - 1; j >= 0; j--) {
     std::vector<Vector3f> pixelRow;
     for (int i = 0; i < width; i++) {
-      auto u = double(i) / (width - 1);
-      auto v = double(j) / (height - 1);
-      Ray r(origin, bottomLeftCorner + u * horizontal + v * vertical - origin);
+      Ray r(Vector3f(0.0, 0.0, 0.0), cam.get_direction(i, j));
 
       Vector3f color = RayColor(r, scene);
       pixelRow.push_back(color);
